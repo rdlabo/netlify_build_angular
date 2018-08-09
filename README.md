d300450e1e77744d52b61caef8a237d90a2fd10f
榊原昌彦 <sakakibara@rdlabo.jp>
2018-08-09 15:25:41 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,62 @@
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+
+# Netlify's build file for Angular Project
+
+This file is Netlify's build file for Angular Project. You can set your environment to this file auto. This will help to increase the lighthouse's score
+
+This file build for 4 method.
+1. create `robot.txt`.
+2. create `_redirects` for redirect to www.
+3. create `_headers` for ServerPush.
+4. change styles inline.
+
+If you use this file, you put this file in package.json's directory.
+And in Netlify's deploy setting, add Build command `node netlify.build.js`
+
+## How to use
+Put `netlify.build.js` in `package.json`'s directory. 
+And in Netlify's deploy setting, add Build command `node netlify.build.js`
+
+## example create file
+
+### create `robot.txt`.
+If your robots.txt file is malformed, crawlers may not be able to understand how you want your website to be crawled or indexed.
+```
+User-Agent: *
+Allow: /
+Host: rdlabo.jp
+```
+
+### create `_redirects` for redirect to www.
+You can use naked domains with Netlify, but we recommend you always use the www version of the domain (eg. www.example.com) for your site. This makes it easier to take advantage of Netlify’s powerful CDN.
+```
+https://rdlabo.jp/*  https://www.rdlabo.jp/:splat  301
+```
+
+### create `_headers` for ServerPush.
+HTTP/2 server push is a performance optimization included in version 2 of the HTTP protocol.
+```
+/*
+  Link: <main.f1ec16ef52a78f4177f7.js>; rel=preload; as=script
+  Link: <polyfills.2f4a59095805af02bd79.js>; rel=preload; as=script
+  Link: <runtime.a66f828dca56eeb90e02.js>; rel=preload; as=script
+```
+
+### change styles inline.
+Resources are blocking the first paint of your page. Consider delivering critical JS/CSS inline and deferring all non-critical JS/styles.
+```index.html
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Default</title>
+  <base href="/">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="H5ph58N7a9jJVFaL63YKNA5lQsM77jcqd5pSFv18I_I" />
+  <link rel="icon" type="image/x-icon" href="favicon.ico"><style></style></head>
+<body>
+  <app-root></app-root>
+<script type="text/javascript" src="runtime.a66f828dca56eeb90e02.js"></script><script type="text/javascript" src="polyfills.2f4a59095805af02bd79.js"></script><script type="text/javascript" src="main.f1ec16ef52a78f4177f7.js"></script></body>
+</html>
+```
---
+++ netlify.build.js
@@ -0,0 +1,94 @@
+/**
+ * This file is Netlify's build file for Angular Project
+ * You can set your environment to this file.
+ *
+ * This file build for 4 method.
+ *   1. create robot.txt.
+ *   2. create _redirects for redirect to www
+ *   3. create _headers for ServerPush
+ *   4. change styles inline
+ *
+ * If you use this file, you put this file in package.json's directory.
+ * And in Netlify's deploy setting, add Build command `node netlify.build.js`
+ */
+
+/***************************************************************************************************
+ * Please change your environment.
+ */
+
+/** Ionic Framework v4 is `www/` */
+const publicDir = 'dist/default/';
+
+/** not add `www` */
+const appDomain = 'rdlabo.jp';
+
+/** create accessable file */
+const debug = true;
+
+/**
+ *  If you need not execute method, please set
+ */
+const execute = {
+  createRobot: true, /** create robot.txt */
+  createRedirects: true, /** create _redirects for redirect to www */
+  createJavaScriptFileServerPush: true, /** create _headers for ServerPush  */
+  changeInlineStyleSheet: true  /** change styles inline */
+};
+
+
+/***************************************************************************************************
+ * Under this line is execute method.
+ */
+
+const fs = require('fs');
+class method {
+  constructor(publicDir, appDomain, debug) {
+    this.publicDir = publicDir;
+    this.appDomain = appDomain;
+    this.debug = debug;
+  }
+
+  createRobot(){
+    const robot = 'User-Agent: *' +
+      'Allow: /' +
+      'Host: ' + this.appDomain;
+    fs.writeFileSync(this.publicDir + 'robot.txt', robot);
+  }
+
+  createRedirects(){
+    const robot = 'https://' + this.appDomain + '/*  https://www.' + this.appDomain + '/:splat  301';
+    fs.writeFileSync(this.publicDir + '_redirects', robot);
+    if (this.debug) fs.writeFileSync(this.publicDir + 'redirects.txt', robot);
+  }
+
+  createJavaScriptFileServerPush(fileArray){
+    let pushFile = '/*\n';
+    fileArray.forEach(file => {
+      pushFile += '  Link: <' + this.__getFileName(file) + '>; rel=preload; as=script\n';
+    });
+    fs.writeFileSync(this.publicDir + '_headers', pushFile);
+    if (this.debug) fs.writeFileSync(this.publicDir + 'headers.txt', pushFile);
+  }
+
+  changeInlineStyleSheet(){
+    const stylesheet = fs.readFileSync(this.publicDir + this.__getFileName('styles')).toString();
+    const indexFile = fs.readFileSync(this.publicDir + 'index.html').toString();
+    const changeString = indexFile.replace(/\n\<link rel=(\"|\')stylesheet(\"|\')[^>]*>/g, '<style>' + stylesheet + '</style>');
+    fs.writeFileSync(this.publicDir + 'index.html', changeString);
+  }
+
+  __getFileName(search){
+    const files = fs.readdirSync(publicDir);
+    const fileList = files.filter((file) => {
+      return (file.match(new RegExp(search)) !== null)
+    });
+
+    return fileList[0];
+  }
+}
+
+const build = new method(publicDir, appDomain, debug);
+if (execute.createRobot) build.createRobot();
+if (execute.createRedirects) build.createRedirects();
+if (execute.createJavaScriptFileServerPush) build.createJavaScriptFileServerPush(['main', 'polyfills', 'runtime']);
+if (execute.changeInlineStyleSheet) build.changeInlineStyleSheet();
